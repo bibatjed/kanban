ff0468c8621e3280c90965304921fe7b5f1890df
Jeeed <43069218+bibatjed@users.noreply.github.com>
2023-01-31 18:27:20 +0800

    Apply dark theme to input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -15,14 +15,14 @@ export default function Input(props: Partial<IProps>) {
   const { placeholder = "e.g Web Design", isReadOnly = false } = props || {};
   return (
     <div className="group w-full">
-      <div className="border-2 relative w-full group-focus-within:border-kanban-main-purple-hover border-kanban-lines-light rounded-md">
+      <div className="border-2 relative w-full group-focus-within:border-kanban-main-purple-hover dark:border-kanban-lines-dark border-kanban-lines-light rounded-md">
         <input
           name={props.name}
           value={props.value}
           onChange={props.onChange}
           placeholder={placeholder}
           readOnly={isReadOnly}
-          className="w-full read-only:pointer-events-none outline-none p-2 rounded-md font-plus-jakarta-sans placeholder-inherit text-sm placeholder-gray-400"
+          className="w-full dark:bg-kanban-dark-grey dark:text-kanban-white read-only:pointer-events-none outline-none p-2 rounded-md font-plus-jakarta-sans placeholder-inherit text-sm placeholder-gray-400"
         />
         <label className="absolute text-kanban-red right-2 font-plus-jakarta-sans text-xs font-semibold capitalize -bottom-0">
           {props.error}
