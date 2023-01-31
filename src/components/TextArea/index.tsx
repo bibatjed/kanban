eee5b58f9b5f53ac155bdf17d56f5326d9c4b1f4
Jeeed <43069218+bibatjed@users.noreply.github.com>
2023-01-31 18:28:49 +0800

    Apply dark theme to textarea

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -10,13 +10,13 @@ interface ITextAreaProps extends TextArea {
 export default function TextArea(props: ITextAreaProps) {
   return (
     <div className="group w-full">
-      <div className="w-full border-2 border-kanban-lines-light rounded-md group-focus-within:border-kanban-main-purple-hover">
+      <div className="w-full border-2 border-kanban-lines-light dark:border-kanban-lines-dark rounded-md group-focus-within:border-kanban-main-purple-hover">
         <textarea
           name={props.name}
           onChange={props.onChange}
           value={props.value}
           placeholder="e.g. It’s always good to take a break. This 15 minute break will recharge the batteries a little."
-          className="w-full p-4 rounded-md font-plus-jakarta-sans h-full placeholder-inherit text-sm outline-none resize-none placeholder-gray-400"
+          className="w-full p-4 dark:text-kanban-white dark:bg-kanban-dark-grey rounded-md font-plus-jakarta-sans h-full placeholder-inherit text-sm outline-none resize-none placeholder-gray-400"
         />
       </div>
     </div>
