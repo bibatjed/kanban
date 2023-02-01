c3ff9bc08d00e972f4cd6f95f826bfe27d4faba3
Jeeed <43069218+bibatjed@users.noreply.github.com>
2023-02-01 13:21:36 +0800

    Move business logic to useBoardModal

---
+++ src/components/BoardModal/hooks/useBoardModal.ts
@@ -0,0 +1,134 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+
+import { formErrors } from '../../../constants';
+
+const { REQUIRED, USED } = formErrors;
+type BoardFormError = {
+  boardName: string;
+  columns: {
+    [key: number]: string;
+  };
+};
+type Columns = {
+  old: string | null;
+  new: string;
+};
+export type BoardFormValues = {
+  boardName: string;
+  columns: Columns[];
+};
+export default function useBoardModal(
+  board: BoardFormValues,
+  boardNames: string[],
+  isOpen: boolean = false
+) {
+  const [formValues, setFormValues] = useState<BoardFormValues>({
+    boardName: '',
+    columns: [],
+  });
+
+  const [errorValues, setErrorValues] = useState<BoardFormError>({
+    boardName: '',
+    columns: {},
+  });
+  useEffect(() => {
+    if (isOpen === true) {
+      setFormValues(board);
+      setErrorValues({ boardName: '', columns: {} });
+    }
+  }, [isOpen, board.boardName]);
+
+  function handleAddColumn() {
+    setFormValues((prev) => {
+      const columns = [...prev.columns];
+      columns.push({
+        old: null,
+        new: '',
+      });
+      return {
+        ...prev,
+        columns,
+      };
+    });
+  }
+
+  function handleOnChangeCommon(e: ChangeEvent<HTMLInputElement>) {
+    setFormValues((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+  function handleOnChangeArray(e: ChangeEvent<HTMLInputElement>) {
+    const id = e.currentTarget.getAttribute('data-id');
+
+    console.log(id);
+    setFormValues((prev) => {
+      const columns = [...prev.columns];
+      columns[Number(id)].new = e.target.value;
+      return {
+        ...prev,
+        columns,
+      };
+    });
+  }
+
+  function handleDeleteColumn(index: number) {
+    setFormValues((prev) => {
+      const columns = [...prev.columns];
+      columns.splice(index, 1);
+      return {
+        ...prev,
+        columns: columns,
+      };
+    });
+  }
+
+  function checkColumnFields() {
+    const formError: BoardFormError = {
+      boardName: '',
+      columns: {},
+    };
+    if (formValues.boardName == '') {
+      formError.boardName = REQUIRED;
+    }
+
+    if (boardNames.includes(formValues.boardName.toLowerCase())) {
+      formError.boardName = USED;
+    }
+
+    formValues.columns.forEach((value, index) => {
+      if (value.new === '' || value.new == null) {
+        formError.columns[index] = REQUIRED;
+        return;
+      }
+
+      const findDuplicate = formValues.columns.findIndex(
+        (column) => column.new === value.new
+      );
+
+      if (findDuplicate >= 0 && findDuplicate !== index) {
+        formError.columns[index] = USED;
+        return;
+      }
+    });
+
+    if (
+      formError.boardName === '' &&
+      Object.keys(formError.columns).length === 0
+    ) {
+      return true;
+    }
+
+    setErrorValues(formError);
+  }
+
+  return {
+    formValues,
+    errorValues,
+    handleAddColumn,
+    handleOnChangeCommon,
+    handleOnChangeArray,
+    handleDeleteColumn,
+    checkColumnFields,
+  };
+}
