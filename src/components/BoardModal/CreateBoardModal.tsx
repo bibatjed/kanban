666487e8f8e64abf80ea16de8fc53af91af018d3
Jeeed <43069218+bibatjed@users.noreply.github.com>
2023-02-01 13:22:24 +0800

    Integrate useBoardModal to createboardmodal component

--- src/components/BoardModal/CreateBoardModal.tsx
+++ src/components/BoardModal/CreateBoardModal.tsx
@@ -1,159 +1,42 @@
-import { useState, ChangeEvent, useEffect } from "react";
-import IconAddTaskMobile from "../../assets/icons/IconAddTaskMobile";
-import IconCross from "../../assets/icons/IconCross";
-import { useAppDispatch, useAppSelector } from "../../hooks/redux";
-import {
-  ContainerState,
-  onAddNewBoard,
-  updateBoard,
-} from "../../reducer/board";
-import Button from "../Button/Button";
-import DialogWrapper from "../DialogWrapper";
-import Input from "../Input";
-import { modal } from "../../constants";
-import { closeModal as reducerCloseModal } from "../../reducer/modal";
-import { addDetails } from "../../reducer/boardDetails";
-
-type errorObj = {
-  boardName: string;
-  columns: {
-    [key: number]: string;
-  };
-};
-type Columns = {
-  old: string | null;
-  new: string;
-  error?: string;
-};
-export type BoardFormValues = {
-  boardName: string;
-  boardNameError?: string;
-  columns: Columns[];
-};
+import IconAddTaskMobile from '../../assets/icons/IconAddTaskMobile';
+import IconCross from '../../assets/icons/IconCross';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { onAddNewBoard } from '../../reducer/board';
+import Button from '../Button/Button';
+import DialogWrapper from '../DialogWrapper';
+import Input from '../Input';
+import { modal } from '../../constants';
+import { closeModal as reducerCloseModal } from '../../reducer/modal';
+import { addDetails } from '../../reducer/boardDetails';
+import useBoardModal from './hooks/useBoardModal';
 
 const { CREATE_BOARD } = modal;
 
 export default function CreateBoardModal() {
   const modal = useAppSelector((state) => state.modalReducers);
   const isOpen = modal.isOpen && modal.modalType === CREATE_BOARD;
-  const boardDetails = useAppSelector((state) => state.boardDetailsReducers);
   const state = useAppSelector((state) => state.containerReducers);
-  const container = state[boardDetails.boardSelectedIndex]?.columns ?? [];
   const boardNames = state.map((item) => item.name.toLowerCase());
   const dispatch = useAppDispatch();
-  const [formValues, setFormValues] = useState<BoardFormValues>({
-    boardName: "",
-    columns: [],
-  });
 
-  useEffect(() => {
-    if (modal.isOpen === true && modal.modalType === CREATE_BOARD)
-      setFormValues((_) => {
-        return {
-          boardName: "",
-          columns: [
-            {
-              old: null,
-              new: "",
-            },
-          ],
-        };
-      });
-  }, [modal.isOpen]);
+  const {
+    formValues,
+    errorValues,
+    handleAddColumn,
+    handleOnChangeCommon,
+    handleOnChangeArray,
+    handleDeleteColumn,
+    checkColumnFields,
+  } = useBoardModal(
+    {
+      boardName: '',
+      columns: [{ old: null, new: '' }],
+    },
+    boardNames,
+    isOpen
+  );
 
-  function handleAddColumn() {
-    setFormValues((prev) => {
-      const columns = [...prev.columns];
-      columns.push({
-        old: null,
-        new: "",
-      });
-      return {
-        ...prev,
-        columns,
-      };
-    });
-  }
-
-  function handleOnChangeCommon(e: ChangeEvent<HTMLInputElement>) {
-    setFormValues((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
-  }
-  function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
-    setFormValues((prev) => {
-      const columns = [...prev.columns];
-      columns[Number(e.target.name)].new = e.target.value;
-      return {
-        ...prev,
-        columns,
-      };
-    });
-  }
-
-  function handleDeleteColumn(index: number) {
-    setFormValues((prev) => {
-      const columns = [...prev.columns];
-      columns.splice(index, 1);
-      return {
-        ...prev,
-        columns: columns,
-      };
-    });
-  }
-
-  function checkColumnFields() {
-    const errorObj: errorObj = {
-      boardName: "",
-      columns: {},
-    };
-
-    if (formValues.boardName == "") {
-      errorObj.boardName = "required";
-    }
-
-    if (boardNames.includes(formValues.boardName.toLowerCase())) {
-      errorObj.boardName = "used";
-    }
-
-    formValues.columns.forEach((value, index) => {
-      if (value.new === "" || value.new == null) {
-        errorObj.columns[index] = "required";
-        return;
-      }
-
-      const findDuplicate = formValues.columns.findIndex(
-        (column, idx) => column.new === value.new
-      );
-
-      if (findDuplicate >= 0 && findDuplicate !== index) {
-        errorObj.columns[index] = "used";
-        return;
-      }
-    });
-
-    if (
-      errorObj.boardName === "" &&
-      Object.keys(errorObj.columns).length === 0
-    ) {
-      return true;
-    }
-
-    setFormValues((prev) => {
-      return {
-        ...prev,
-        boardNameError: errorObj.boardName,
-
-        columns: prev.columns.map((value, index) => {
-          return {
-            ...value,
-            error: errorObj.columns[index] || "",
-          };
-        }),
-      };
-    });
-  }
+  console.log(formValues.columns);
 
   function handleSubmit() {
     if (!checkColumnFields()) {
@@ -161,7 +44,7 @@ export default function CreateBoardModal() {
     }
 
     dispatch(onAddNewBoard(formValues));
-    dispatch(addDetails({ type: "", boardSelectedIndex: boardNames.length }));
+    dispatch(addDetails({ type: '', boardSelectedIndex: boardNames.length }));
     closeModal();
   }
   function closeModal() {
@@ -174,11 +57,11 @@ export default function CreateBoardModal() {
       <div className="mt-2 flex flex-col gap-4">
         {/* BOARD NAME */}
         <div className="flex flex-col gap-2">
-          <span className="font-plus-jakarta-sans text-sm text-kanban-medium-grey font-semibold">
+          <span className="font-plus-jakarta-sans text-sm font-semibold text-kanban-medium-grey">
             Board Name
           </span>
           <Input
-            error={formValues.boardNameError}
+            error={errorValues.boardName}
             name="boardName"
             onChange={handleOnChangeCommon}
             value={formValues.boardName}
@@ -194,11 +77,11 @@ export default function CreateBoardModal() {
             return (
               <div key={index} className="flex flex-row items-center gap-4">
                 <Input
-                  error={value.error}
-                  name={index.toString()}
-                  onChange={handleOnChange}
+                  dataId={index}
+                  error={errorValues.columns[index]}
+                  onChange={handleOnChangeArray}
                   value={value.new}
-                />{" "}
+                />{' '}
                 <div>
                   <Button
                     onClick={() => {
@@ -207,7 +90,7 @@ export default function CreateBoardModal() {
                     text=""
                     variant="none"
                   >
-                    <IconCross className={`${"fill-kanban-medium-grey"}`} />
+                    <IconCross className={`${'fill-kanban-medium-grey'}`} />
                   </Button>
                 </div>
               </div>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,11 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent } from 'react';
 
 type InputProps = {
   isReadOnly: boolean;
   placeholder: string;
   value: string;
-  name: string;
+  name?: string;
+  dataId?: number;
   error?: string;
 };
 
@@ -12,20 +13,30 @@ interface IProps extends InputProps {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 export default function Input(props: Partial<IProps>) {
-  const { placeholder = "e.g Web Design", isReadOnly = false } = props || {};
+  const {
+    placeholder = 'e.g Web Design',
+    isReadOnly = false,
+    name,
+    value,
+    dataId,
+    onChange,
+    error,
+  } = props || {};
+
   return (
     <div className="group w-full">
-      <div className="border-2 relative w-full group-focus-within:border-kanban-main-purple-hover dark:border-kanban-lines-dark border-kanban-lines-light rounded-md">
+      <div className="relative w-full rounded-md border-2 border-kanban-lines-light group-focus-within:border-kanban-main-purple-hover dark:border-kanban-lines-dark">
         <input
-          name={props.name}
-          value={props.value}
-          onChange={props.onChange}
+          name={name}
+          value={value}
+          data-id={dataId}
+          onChange={onChange}
           placeholder={placeholder}
           readOnly={isReadOnly}
-          className="w-full dark:bg-kanban-dark-grey dark:text-kanban-white read-only:pointer-events-none outline-none p-2 rounded-md font-plus-jakarta-sans placeholder-inherit text-sm placeholder-gray-400"
+          className="w-full rounded-md p-2 font-plus-jakarta-sans text-sm placeholder-inherit placeholder-gray-400 outline-none read-only:pointer-events-none dark:bg-kanban-dark-grey dark:text-kanban-white"
         />
-        <label className="absolute text-kanban-red right-2 font-plus-jakarta-sans text-xs font-semibold capitalize -bottom-0">
-          {props.error}
+        <label className="absolute right-2 -bottom-0 font-plus-jakarta-sans text-xs font-semibold capitalize text-kanban-red">
+          {error}
         </label>
       </div>
     </div>
